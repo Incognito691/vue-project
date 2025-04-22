dbbd3bb8c0d47a0fda9ad8eec7874aaed6066427
sahil niraula <sahil.niraula00@gmail.com>
2025-04-22 14:56:15 +0545

    feat: added contiontinal rendering and removed the previous codes

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,17 @@
   <main class="flex justify-center dark:bg-gray-300 min-h-screen p-4 bg-gray-50 overflow-y-hidden">
     <div class="max-w-5xl w-full flex bg-white h-full p-5 rounded-lg shadow-lg">
       <div>
-        <h1>{{ greet }} {{ message }}</h1>
-        <h1 v-text="name"></h1>
-        <h1 v-html="boldText"></h1>
-        <h2 :id="headingId">This is a heading</h2>
-        <h1 :style="{
-          color: highlight,
-          fontSize: headerSize + 'px',
-        }">Instyle Style</h1>
-        <h1 :style="styleObject">Style Object</h1>
-        <button :disabled="isDisabled" class="bg-gray-300 p-2 rounded-lg">Disabled</button>
-        <div :style="[baseStyleObject, successStyleObject]">Success Style</div>
-        <div :style="[baseStyleObject, dangerStyleObject]">Danger Style</div>
+        <h1 v-if="num === 5"> This is number <span span class=" text-2xl text-green-500"> {{ num }} </span>
+        </h1>
+        <h1 v-else-if="num > 5">It is greater than five. The number is <span class="text-2xl text-green-500"> {{
+          num }} </span> </h1>
+        <h1 v-else-if="num < 5">It is less than five. The number is <span class="text-2xl text-green-500"> {{
+          num }} </span> </h1>
+        <h1 v-else><span class="text-2xl text-red-500">Please choose a number. Not "{{ num }}" </span>.
+        </h1>
+        <template v-if="display">
+          <h1 class="text-2xl text-gray-300">This is a hidden message</h1>
+        </template>
       </div>
     </div>
   </main>
@@ -21,41 +20,12 @@
 
 
 <script>
-
 export default {
   name: 'App',
   data() {
     return {
-      message: 'Vue!',
-      greet: "Hello",
-      name: "Sahil",
-      boldText: "<b>This is a bold text</b>",
-      headingId: "heading",
-      isDisabled: true,
-      highlight: "orange",
-      headerSize: 100,
-      styleObject: {
-        color: 'red',
-        fontSize: '100px',
-        textDecoration: "bold"
-      },
-      baseStyleObject: {
-        fontSize: '100px',
-        padding: '5px',
-        margin: '5px',
-      },
-      successStyleObject: {
-        color: 'green',
-        backgroundColor: 'lightgreen',
-        border: '1px solid green',
-        borderRadius: '5px',
-      },
-      dangerStyleObject: {
-        color: 'darkred',
-        backgroundColor: 'lightcoral',
-        border: '1px solid red',
-        borderRadius: '5px',
-      }
+      num: "Hello",
+      display: true,
     }
   }
 }
